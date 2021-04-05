b04bde40ebaf28834e7adde837547a828ed2f5ea
Benoit Pierre <benoit.pierre@gmail.com>
2021-04-05 23:51:27 +0200

    add wordlist and fill-in orthographic rules

--- plover_michela/system.py
+++ plover_michela/system.py
@@ -14,11 +14,75 @@ NUMBERS = {}
 
 UNDO_STROKE_STENO = 'U'
 
-ORTHOGRAPHY_RULES = []
+ORTHOGRAPHY_RULES = [
+    (r'^(.*). \^ cela$', r'\1cela'),
+    (r'^(.*). \^ cele$', r'\1cele'),
+    (r'^(.*). \^ celi$', r'\1celi'),
+    (r'^(.*). \^ celo$', r'\1celo'),
+
+    (r'^(.*). \^ cene$', r'\1cene'),
+
+    (r'^(.*). \^ ci$', r'\1ci'),
+
+    (r'^(.*). \^ dola$', r'\1dola'),
+    (r'^(.*). \^ dole$', r'\1dole'),
+    (r'^(.*). \^ doli$', r'\1doli'),
+    (r'^(.*). \^ dolo$', r'\1dolo'),
+
+    (r'^(.*). \^ done$', r'\1done'),
+
+    (r'^(.*). \^ gli$', r'\1gli'),
+
+    (r'^(.*). \^ la$', r'\1la'),
+    (r'^(.*). \^ le$', r'\1le'),
+    (r'^(.*). \^ li$', r'\1li'),
+    (r'^(.*). \^ lo$', r'\1lo'),
+
+    (r'^(.*). \^ mela$', r'\1mela'),
+    (r'^(.*). \^ mele$', r'\1mele'),
+    (r'^(.*). \^ meli$', r'\1meli'),
+    (r'^(.*). \^ melo$', r'\1melo'),
+
+    (r'^(.*). \^ mene$', r'\1mene'),
+    (r'^(.*). \^ mente$', r'\1mente'),
+    (r'^(.*). \^ mente$', r'\1amente'),
+
+    (r'^(.*). \^ mi$', r'\1mi'),
+
+    (r'^(.*). \^ ne$', r'\1ne'),
+
+    (r'^(.*). \^ sela$', r'\1sela'),
+    (r'^(.*). \^ sele$', r'\1sele'),
+    (r'^(.*). \^ seli$', r'\1seli'),
+    (r'^(.*). \^ selo$', r'\1selo'),
+
+    (r'^(.*). \^ si$', r'\1si'),
+
+    (r'^(.*). \^ tela$', r'\1tela'),
+    (r'^(.*). \^ tele$', r'\1tele'),
+    (r'^(.*). \^ teli$', r'\1teli'),
+    (r'^(.*). \^ telo$', r'\1telo'),
+
+    (r'^(.*). \^ ti$', r'\1ti'),
+
+    (r'^(.*). \^ vela$', r'\1vela'),
+    (r'^(.*). \^ vele$', r'\1vele'),
+    (r'^(.*). \^ veli$', r'\1veli'),
+    (r'^(.*). \^ velo$', r'\1velo'),
+
+    (r'^(.*). \^ vi$', r'\1vi'),
+
+    (r'^(.*). \^ issima$', r'\1issima'),
+    (r'^(.*). \^ issime$', r'\1issime'),
+    (r'^(.*). \^ issimi$', r'\1issimi'),
+    (r'^(.*). \^ issimo$', r'\1issimo'),
+
+    (r'^(.*). \^ i$', r'\1i'),
+]
 
 ORTHOGRAPHY_RULES_ALIASES = {}
 
-ORTHOGRAPHY_WORDLIST = None
+ORTHOGRAPHY_WORDLIST = 'italian_words.txt'
 
 KEYMAPS = {
     'Michela Keyboard (MIDI, 2 octaves)': {
